941d5577a7aab2f99e9af875f497ebadd7cd5d82
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-15 18:16:37 -0300

    feat: Creating just a header for export.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import './App.css';
-import Logo from "./components/Logo/index.js";
-import perfil from "./images/perfil.svg" 
-import sacola from "./images/sacola.svg"
-
-const optionText = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
-const icons = [perfil, sacola]
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className = "App">
-       <header className='App-header'>
-           <Logo></Logo>
-         <ul className ='opcoes'>
-              { optionText.map( (texto) => (
-                <li className = 'opcao'><p>{texto}</p></li>
-              ) ) }
-           </ul>
-
-          <ul className='icons'>
-               { icons.map( (icon) => (
-                 <li className='icon'><img src={icon}></img></li>
-                )) }
-          </ul>
-        </header>
-    </div>
+    <Header/>
   );
 }
 
