db86b93ba7a703cf2a0c09ea1f082b4cc0d33e24
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-15 17:34:30 -0300

    feat: Aplicating the components and map.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
 import Logo from "./components/Logo/index.js";
+import perfil from "./images/perfil.svg" 
+import sacola from "./images/sacola.svg"
+
+const optionText = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icons = [perfil, sacola]
 
 function App() {
   return (
     <div className = "App">
-      <header className='App-header'>
-       <Logo></Logo>
-      </header>
+       <header className='App-header'>
+           <Logo></Logo>
+         <ul className ='opcoes'>
+              { optionText.map( (texto) => (
+                <li className = 'opcao'><p>{texto}</p></li>
+              ) ) }
+           </ul>
+
+          <ul className='icons'>
+               { icons.map( (icon) => (
+                 <li className='icon'><img src={icon}></img></li>
+                )) }
+          </ul>
+        </header>
     </div>
   );
 }
 
 export default App;
+
