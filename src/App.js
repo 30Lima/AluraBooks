02dbac6367e03e21f47fa94da9c32f6052bc26a3
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-15 13:41:25 -0300

    feat: Adding the new component in page.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import logo from './images/logo.svg'
+import Logo from "./components/Logo/index.js";
 
 function App() {
   return (
     <div className = "App">
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura</strong>Books</p>
-
-        </div>
+       <Logo></Logo>
       </header>
     </div>
   );
