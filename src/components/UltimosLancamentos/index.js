89bfbf7d01a63f454f05efd25867d10082f68b86
30lima <pedrolimaa354@gmail.com>
2025-02-22 15:28:33 -0300

    feat: Aplicating styled component to in file.

--- src/components/UltimosLancamentos/index.js
+++ src/components/UltimosLancamentos/index.js
@@ -1,13 +1,42 @@
 import { livros } from './dadosUltimosLancamentos'
+import styled from 'styled-components'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const Titulo = styled.h2`
+    width: 100%;
+    padding: 30px 0;
+    background-color: #FFF;
+    color: #EB9B00;
+    font-size: 36px;
+    text-align: center;
+    margin: 0;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
 
 export function UltimosLancamentos () {
     return (
-            <section>
-                <h2>Ultimos Lançamentos</h2>
-                {livros.map( livros => (
-                    <img src = {livros.src}/>
-                ))}
-            </section>
+            <UltimosLancamentosContainer>
+            <Titulo>ULTIMOS LANCAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                { livros.map( livro => (
+                    <img src={livro.src}/>
+                ) ) }
+            </NovosLivrosContainer>
+        </UltimosLancamentosContainer>
     )
 }
 
